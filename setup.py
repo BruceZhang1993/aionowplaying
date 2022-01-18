907a53bafd7b4e3d0788abc27e4380357b610a4c
Bruce Zhang <zttt183525594@gmail.com>
2022-01-18 14:52:01 +0800

    dependencies

---
+++ setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['aionowplaying', 'aionowplaying.interface']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pydantic']
+
+extras_require = \
+{':sys_platform == "darwin"': ['pyobjc'],
+ ':sys_platform == "linux"': ['dbus-next'],
+ ':sys_platform == "win32"': ['winrt']}
+
+setup_kwargs = {
+    'name': 'aionowplaying',
+    'version': '0.9.0',
+    'description': 'A cross-platform Now Playing client',
+    'long_description': '',
+    'author': 'Bruce Zhang',
+    'author_email': 'zttt183525594@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/BruceZhang1993/aionowplaying',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.7,<3.10',
+}
+
+
+setup(**setup_kwargs)
+
