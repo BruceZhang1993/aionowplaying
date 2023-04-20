e7f096a653b09c67f608f1b032ed9cba50f97bd2
Bruce Zhang <zttt183525594@gmail.com>
2023-04-20 17:48:46 +0800

    bump version 0.9.4

--- setup.py
+++ setup.py
@@ -11,13 +11,14 @@ install_requires = \
 ['pydantic']
 
 extras_require = \
-{':sys_platform == "darwin"': ['pyobjc-framework-MediaPlayer', 'pyobjc-framework-Cocoa'],
+{':sys_platform == "darwin"': ['pyobjc-framework-MediaPlayer',
+                               'pyobjc-framework-Cocoa'],
  ':sys_platform == "linux"': ['dbus-next'],
  ':sys_platform == "win32"': ['winsdk']}
 
 setup_kwargs = {
     'name': 'aionowplaying',
-    'version': '0.9.3',
+    'version': '0.9.4',
     'description': 'A cross-platform Now Playing client',
     'long_description': '',
     'author': 'Bruce Zhang',
@@ -29,7 +30,7 @@ setup_kwargs = {
     'package_data': package_data,
     'install_requires': install_requires,
     'extras_require': extras_require,
-    'python_requires': '>=3.7,<=3.12',
+    'python_requires': '>=3.7,<3.12',
 }
 
 
