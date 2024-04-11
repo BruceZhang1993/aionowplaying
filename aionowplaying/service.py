aaebc59bf23e4d4cf2ddf534e546efe7ab875137
Bruce Zhang <zttt183525594@gmail.com>
2024-04-11 19:34:35 +0800

    service

--- aionowplaying/service.py
+++ aionowplaying/service.py
@@ -1,14 +1,15 @@
 from threading import Thread
 
 from aionowplaying.backend import create_backend
-from aionowplaying.interface import MPInterface
+from aionowplaying.interface import MPInterface, MPPlayerInterface, MPTrackListInterface
 
 
 class NowPlayingService(Thread):
-    def __init__(self, interface: MPInterface):
+    def __init__(self, interface: MPInterface, player_interface: MPPlayerInterface,
+                 tracklist_interface: MPTrackListInterface):
         super().__init__()
         self._interface = interface
-        self._backend = create_backend(interface)
+        self._backend = create_backend(interface, player_interface, tracklist_interface)
 
     def run(self):
         self._backend.run()
